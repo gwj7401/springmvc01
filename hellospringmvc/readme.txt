45bbb63f62aa75802a0cc6858170dc7826347f66
gwj7401 <gwjyyl@126.com>
2020-09-15 16:51:54 +0800

    Initial commit

---
+++ hellospringmvc/readme.txt
@@ -0,0 +1,5 @@
+第一个springmvc
+实现步骤：
+1、新建一个maven的project工程
+    加入依赖 spring-webmvc，会把spring的依赖都加入到项目
+    jsp sevlet依赖
---
+++ hellospringmvc/src/main/java/net/gicp/controller/MyController.java
@@ -0,0 +1,35 @@
+package net.gicp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author yanghaokun guoweijiang
+ * @create 2020-09-15 - 15:23
+ * @Controller 创建控制器对象，对象放在SpringMvc容器里
+ */
+@Controller
+public class MyController {
+    /**
+     * 处理用户提交的请求，springmvc是使用方法来处理的
+     * 方法是自定义的，可以有多种返回值，多种参数
+    */
+    /**
+     * 准备使用doSome方法来处理some.do请求
+     */
+    @RequestMapping(value = "/some.do")
+    public ModelAndView doSome(){
+
+         ModelAndView mv = new ModelAndView();
+         //添加数据
+    mv.addObject("msg","欢迎使用SpringMvc做web开发");
+    mv.addObject("fun","执行的是doSome方法");
+    //指定视图
+    mv.setViewName("/show.jsp");
+    return mv;
+
+    }
+
+}
+
